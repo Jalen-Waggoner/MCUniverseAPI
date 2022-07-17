da0c68540aaeabe2a9badd1c6f64ff8acf7eff84
Jalen Waggoner <jalenwaggoner@gmail.com>
2022-07-17 00:04:21 -0400

    Initial Commit

---
+++ MCUniverse.Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MCUniverse.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+}
+        
