eed7c0435705a548cba56152b031c3fa4901db5f
RiaKaramdad2022 <floriakaramdad2022@gmail.com>
2022-07-18 11:21:55 -0400

    Added Student Entity

--- MCUniverse.Data/Entities/Student.cs
+++ MCUniverse.Data/Entities/Student.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,21 @@ namespace MCUniverse.Data.Entities
 {
     public class Student
     {
-        
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FullName { get; set; } = null!;
+        [Required]
+        public string Gender { get; set; } = null!;
+        [Required]
+        public string Address { get; set; } = null!;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+        [Required]
+        public int PhoneNumber { get; set; }
+        [Required]
+        public string OriginCountry { get; set; } = null!;
     }
+    public List<CourseEntity> courses { get; set; } = new List<CourseEntity>();
 }
