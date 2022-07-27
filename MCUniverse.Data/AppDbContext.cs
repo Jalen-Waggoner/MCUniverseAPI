84302a6ceefc879b3de4492b19cfd6da901293e6
Jalen Waggoner <jalenwaggoner@gmail.com>
2022-07-27 19:21:28 -0400

    Added Seed Data for Initial Create

--- MCUniverse.Data/AppDbContext.cs
+++ MCUniverse.Data/AppDbContext.cs
@@ -14,6 +14,31 @@ public class AppDbContext : DbContext
     public DbSet<Faculty> Faculties { get; set; }
     public DbSet<Student> Students { get; set; }
 
-}
- 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Faculty>()
+            .HasData(
+            new Faculty { Id = 1, UserName = "NicholasFury", Password = "EyePatch", FirstName = "Nicholas", LastName = "Fury", Email = "NickFury@MCUniversity.com", PhoneNumber = "(555)-650-6639", Gender = 0 },
+            new Faculty { Id = 2, UserName = "TheTitan", Password = "50/50", FirstName = "Thanos", LastName = "Purpleman", Email = "Thanos@MCUniversity.com", PhoneNumber = "(555)-242-3649", Gender = 0 },
+            new Faculty { Id = 3, UserName = "JaneFoster", Password = "ThorsAHunk", FirstName = "Jane", LastName = "Foster", Email = "JaneFoster@MCUniversity.com", PhoneNumber = "(555)-242-1277", Gender = (GenderEnum)1 },
+            new Faculty { Id = 4, UserName = "AbrahamErskine", Password = "AmericaForever", FirstName = "Abraham", LastName = "Erskine", Email = "AbrahamErskine@MCUniversity.com", PhoneNumber = "(555)-242-4847", Gender = 0 },
+            new Faculty { Id = 5, UserName = "ProfSelvig", Password = "UnderwearMan", FirstName = "Professor", LastName = "Selvig", Email = "ProfSelvig@MCUniversity.com", PhoneNumber = "(555)-532-5100", Gender = 0 });
 
+        modelBuilder.Entity<CourseEntity>()
+            .HasData(
+            new CourseEntity { Id = 1, FacultyId = 1, Name = "Saving the World 101", StartTime = "9:00", EndTime = "10:00", ClassDays = "Monday, Wednesday, Friday", Credits = 3, Semester = 1, Building = "Philip Colson Memorial Hall", RoomNumber = 201 },
+            new CourseEntity { Id = 2, FacultyId = 2, Name = "Metal Hearts 101", StartTime = "10:00", EndTime = "11:00", ClassDays = "Wednesday", Credits = 5, Semester = 1, Building = "Philip Colson Memorial Hall", RoomNumber = 201 },
+            new CourseEntity { Id = 3, FacultyId = 3, Name = "I Am Groot", StartTime = "11:00", EndTime = "12:00", ClassDays = "Monday, Friday", Credits = 3, Semester = 2, Building = "Falcon's Nest", RoomNumber = 125 },
+            new CourseEntity { Id = 4, FacultyId = 4, Name = "Infinity Stones 201", StartTime = "2:15", EndTime = "4:00", ClassDays = "Tuesday, Thursday", Credits = 3, Semester = 0, Building = "Philip Colson Memorial Hall", RoomNumber = 211 },
+            new CourseEntity { Id = 5, FacultyId = 5, Name = "Catch Phrases 301", StartTime = "9:00", EndTime = "10:00", ClassDays = "Monday, Wednesday, Friday", Credits = 3, Semester = 1, Building = "Philip Colson Memorial Hall", RoomNumber = 154 },
+            new CourseEntity { Id = 6, FacultyId = 3, Name = "Catch Phrases 101", StartTime = "3:00", EndTime = "4:00", ClassDays = "Thursday", Credits = 7, Semester = 1, Building = "Bifrost Keep", RoomNumber = 510 });
+
+        modelBuilder.Entity<Student>()
+            .HasData(
+            new Student { Id = 1, Username = "SteveRogers", Password = "FREEDOM", DateOfBirth = new DateTime(1910 - 07 - 27), FullName = "Steven Rogers", Gender = "Male", Address = "1600 Pennsylvania Ave NW", Email = "SteveRogers@MCUniversity.com", PhoneNumber = "(555)-650-1234", OriginCountry = "United States of America", DateCreated = DateTime.Now, LastModified = DateTime.Now },
+            new Student { Id = 2, Username = "BuckyBarnes", Password = "Soldier76", DateOfBirth = new DateTime(1905 - 12 - 27), FullName = "Bucky Barnes", Gender = "Male", Address = "846 Wakanda Ln", Email = "BuckyBoy@MCUniversity.com", PhoneNumber = "(555)-514-6542", OriginCountry = "United States of America", DateCreated = DateTime.Now, LastModified = DateTime.Now },
+            new Student { Id = 3, Username = "SamWilly", Password = "CapForever", DateOfBirth = new DateTime(1983 - 07 - 26), FullName = "Samuel Wilson", Gender = "Male", Address = "355 Taldore Dr", Email = "WingMan@MCUniversity.com", PhoneNumber = "(555)-675-2548", OriginCountry = "United States of America", DateCreated = DateTime.Now, LastModified = DateTime.Now },
+            new Student { Id = 4, Username = "PeterPiper", Password = "SpiderGuy", DateOfBirth = new DateTime(2005 - 10 - 07), FullName = "Peter Parker", Gender = "Male", Address = "658 Dog Breath Blvd", Email = "SpidyGuy@MCUniversity.com", PhoneNumber = "(555)-654-9873", OriginCountry = "United States of America", DateCreated = DateTime.Now, LastModified = DateTime.Now },
+            new Student { Id = 5, Username = "WandaWitchin", Password = "MissYouVision", DateOfBirth = new DateTime(1993 - 09 - 15), FullName = "Wanda Maximoff", Gender = "Female", Address = "486 CrackBaby Ave", Email = "ScarletBitch@MCUniversity.com", PhoneNumber = "(555)-127-1237", OriginCountry = "Sokovia", DateCreated = DateTime.Now, LastModified = DateTime.Now });
+    }
+}
---
+++ MCUniverse.Data/Migrations/20220727231638_SeedData.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MCUniverse.Data.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Faculties",
+                columns: new[] { "Id", "Email", "FirstName", "Gender", "LastName", "Password", "PhoneNumber", "UserName" },
+                values: new object[,]
+                {
+                    { 1, "NickFury@MCUniversity.com", "Nicholas", 0, "Fury", "EyePatch", "(555)-650-6639", "NicholasFury" },
+                    { 2, "Thanos@MCUniversity.com", "Thanos", 0, "Purpleman", "50/50", "(555)-242-3649", "TheTitan" },
+                    { 3, "JaneFoster@MCUniversity.com", "Jane", 1, "Foster", "ThorsAHunk", "(555)-242-1277", "JaneFoster" },
+                    { 4, "AbrahamErskine@MCUniversity.com", "Abraham", 0, "Erskine", "AmericaForever", "(555)-242-4847", "AbrahamErskine" },
+                    { 5, "ProfSelvig@MCUniversity.com", "Professor", 0, "Selvig", "UnderwearMan", "(555)-532-5100", "ProfSelvig" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Address", "DateCreated", "DateOfBirth", "Email", "FullName", "Gender", "LastModified", "OriginCountry", "Password", "PhoneNumber", "Username" },
+                values: new object[,]
+                {
+                    { 1, "1600 Pennsylvania Ave NW", new DateTime(2022, 7, 27, 19, 16, 38, 638, DateTimeKind.Local).AddTicks(4294), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified).AddTicks(1876), "SteveRogers@MCUniversity.com", "Steven Rogers", "Male", new DateTime(2022, 7, 27, 19, 16, 38, 638, DateTimeKind.Local).AddTicks(4324), "United States of America", "FREEDOM", "(555)-650-1234", "SteveRogers" },
+                    { 2, "846 Wakanda Ln", new DateTime(2022, 7, 27, 19, 16, 38, 638, DateTimeKind.Local).AddTicks(4327), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified).AddTicks(1866), "BuckyBoy@MCUniversity.com", "Bucky Barnes", "Male", new DateTime(2022, 7, 27, 19, 16, 38, 638, DateTimeKind.Local).AddTicks(4328), "United States of America", "Soldier76", "(555)-514-6542", "BuckyBarnes" },
+                    { 3, "355 Taldore Dr", new DateTime(2022, 7, 27, 19, 16, 38, 638, DateTimeKind.Local).AddTicks(4330), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified).AddTicks(1950), "WingMan@MCUniversity.com", "Samuel Wilson", "Male", new DateTime(2022, 7, 27, 19, 16, 38, 638, DateTimeKind.Local).AddTicks(4331), "United States of America", "CapForever", "(555)-675-2548", "SamWilly" },
+                    { 4, "658 Dog Breath Blvd", new DateTime(2022, 7, 27, 19, 16, 38, 638, DateTimeKind.Local).AddTicks(4333), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified).AddTicks(1988), "SpidyGuy@MCUniversity.com", "Peter Parker", "Male", new DateTime(2022, 7, 27, 19, 16, 38, 638, DateTimeKind.Local).AddTicks(4335), "United States of America", "SpiderGuy", "(555)-654-9873", "PeterPiper" },
+                    { 5, "486 CrackBaby Ave", new DateTime(2022, 7, 27, 19, 16, 38, 638, DateTimeKind.Local).AddTicks(4337), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified).AddTicks(1969), "ScarletBitch@MCUniversity.com", "Wanda Maximoff", "Female", new DateTime(2022, 7, 27, 19, 16, 38, 638, DateTimeKind.Local).AddTicks(4338), "Sokovia", "MissYouVision", "(555)-127-1237", "WandaWitchin" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Courses",
+                columns: new[] { "Id", "Building", "ClassDays", "Credits", "EndTime", "FacultyId", "Name", "RoomNumber", "Semester", "StartTime" },
+                values: new object[,]
+                {
+                    { 1, "Philip Colson Memorial Hall", "Monday, Wednesday, Friday", 3, "10:00", 1, "Saving the World 101", 201, 1, "9:00" },
+                    { 2, "Philip Colson Memorial Hall", "Wednesday", 5, "11:00", 2, "Metal Hearts 101", 201, 1, "10:00" },
+                    { 3, "Falcon's Nest", "Monday, Friday", 3, "12:00", 3, "I Am Groot", 125, 2, "11:00" },
+                    { 4, "Philip Colson Memorial Hall", "Tuesday, Thursday", 3, "4:00", 4, "Infinity Stones 201", 211, 0, "2:15" },
+                    { 5, "Philip Colson Memorial Hall", "Monday, Wednesday, Friday", 3, "10:00", 5, "Catch Phrases 301", 154, 1, "9:00" },
+                    { 6, "Bifrost Keep", "Thursday", 7, "4:00", 3, "Catch Phrases 101", 510, 1, "3:00" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
